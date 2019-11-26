7aadbf84eb03cf96e96363294aae6fc923f79640
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2019-11-26 00:19:16 -0500

    added Home, My Team, Players, and Teams to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,22 +21,26 @@ function NavBar() {
 
 
   return (
-    // <div className="nav-container">
-    //   <header className="navlinks">
-    //     <Link className="button" to="/">
-    //       Home
-    //     </Link>
-    //     <Link className="button" to="/teamstats">
-    //       Team Stats
-    //     </Link>
-    //     <Link className="button" to="/playerstats">Shot Charts</Link>
-    //     <Link className="button" to="/teamstats">Login</Link>
-    //   </header>
-    // </div>
+    <>
+    {/* <div className="nav-container">
+      <header className="navlinks">
+        <Link className="button" to="/">
+          Home
+        </Link>
+        <Link className="button" to="/teamstats">
+          Team Stats
+        </Link>
+        <Link className="button" to="/playerstats">Shot Charts</Link>
+        <Link className="button" to="/teamstats">Login</Link>
+      </header>
+    </div> */}
+
+
     <Menu onClick={handleClick} selectedKeys={[menu.current]} mode="horizontal">
     <Menu.Item key="mail">
-      <Icon type="mail" />
-      Home
+      <Link to="/">
+          Home
+        </Link>
     </Menu.Item>
 
     <Menu.Item key="alipay">
@@ -47,44 +51,37 @@ function NavBar() {
     <SubMenu
       title={
         <span className="submenu-title-wrapper">
-          <Icon type="setting" />
+          <Icon type="user" />
           Players
         </span>
       }
     >
-      <Menu.ItemGroup title="">
-        <Menu.Item key="setting:1">Shot Charts</Menu.Item>
+      <Menu.ItemGroup title="Player Stats">
+        <Menu.Item key="setting:1"><Link to="/playerstats">Shot Charts</Link></Menu.Item>
         <Menu.Item key="setting:2">Per Possession Stats</Menu.Item>
         <Menu.Item key="setting:2">Per Game Stats</Menu.Item>
-        <Menu.Item key="setting:2">Season Leaders</Menu.Item>
-      </Menu.ItemGroup>
-      <Menu.ItemGroup title="Team Stats">
-        <Menu.Item key="setting:3">Team Comparisons</Menu.Item>
-        <Menu.Item key="setting:4">Per Possession Stats</Menu.Item>
         <Menu.Item key="setting:2">Season Leaders</Menu.Item>
       </Menu.ItemGroup>
     </SubMenu>
     <SubMenu
       title={
         <span className="submenu-title-wrapper">
-          <Icon type="setting" />
+          <Icon type="team" />
           Teams
         </span>
       }
     >
-      <Menu.ItemGroup title="">
-        <Menu.Item key="setting:1">Shot Charts</Menu.Item>
-        <Menu.Item key="setting:2">Per Possession Stats</Menu.Item>
-        <Menu.Item key="setting:2">Per Game Stats</Menu.Item>
-        <Menu.Item key="setting:2">Season Leaders</Menu.Item>
-      </Menu.ItemGroup>
+
       <Menu.ItemGroup title="Team Stats">
-        <Menu.Item key="setting:3">Team Comparisons</Menu.Item>
+        <Menu.Item key="setting:3"><Link to="/teamstats">
+          Team Comparisons
+        </Link></Menu.Item>
         <Menu.Item key="setting:4">Per Possession Stats</Menu.Item>
         <Menu.Item key="setting:2">Season Leaders</Menu.Item>
       </Menu.ItemGroup>
     </SubMenu>
   </Menu>
+  </>
   );
 }
 
